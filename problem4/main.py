cd0f87359fee17798e1f7bd9953439cc80c26794
ahmadalpadani <ahmadalpadani26@gmail.com>
2024-03-07 11:29:58 +0700

    push

--- problem4/main.py
+++ problem4/main.py
@@ -1,23 +1,9 @@
 def count_item_and_sort(items):
-    # Menghitung jumlah kemunculan setiap item
-    item_count = {}
-    for item in items:
-        if item in item_count:
-            item_count[item] += 1
-        else:
-            item_count[item] = 1
-
-    # Mengurutkan item berdasarkan abjad
-    sorted_items = sorted(set(item_count[item]))
-
-    # Menghasilkan string hasil perhitungan
-    result = ""
-    for item in sorted_items:
-        result += f"{item}->{item_count.get(item, 0)} "
-
-    return result.strip()
-
-
+    data = {}
+    for el in items:
+        data[el] = items.count(el)
+    sorted_data = dict(sorted(data.items(), key=lambda value: (value[1], value[0])))
+    return ' '.join([f"{key}->{value}" for key, value in sorted_data.items()])
 
 if __name__ == "__main__":
     print(count_item_and_sort(["js", "js", "golang", "ruby", "ruby", "js", "js"]))
